3b41c06cf59231ebac83839b6ec4dc549b315dd0
Pvag <paolondon@gmail.com>
2022-11-19 22:32:31 +0100

    feat: use ES6 modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import times from './utils'
+
 const world = 'world'
 
 export function hello(who: string = world): string {
@@ -5,5 +7,8 @@ export function hello(who: string = world): string {
 }
 
 const h1 = hello('Shirley')
-
 console.log(h1)
+
+// use imported code
+const n1 = 2, n2 = 4
+console.log(n1, 'times', n2, 'equals', times(2, 4))
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+// example module exported: you can import it with a regular ES6 import
+
+const times = (factor: number, n: number): number => {
+	return factor * n
+}
+
+export default times
